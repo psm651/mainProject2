e1ed3ef233f4616f1e51ad362f9ec6e3b648567c
horannabi <horangg107@gmail.com>
2019-01-21 14:02:40 +0900

    Horan : 게시물 3인방 단합대회

--- WhatTheHealth/src/main/java/com/wthealth/controller/MainController.java
+++ WhatTheHealth/src/main/java/com/wthealth/controller/MainController.java
@@ -28,89 +28,89 @@ import com.wthealth.service.main.MainService;
 import com.wthealth.service.meeting.MeetingService;
 
 @Controller
-@RequestMapping("/")
+@RequestMapping("/*")
 public class MainController {
 
-	//Field
-	@Autowired
-	@Qualifier("mainServiceImpl")
-	private MainService mainService;
-	@Autowired
-	@Qualifier("exInfoServiceImpl")
-	private ExInfoService exInfoService;
-	@Autowired
-	@Qualifier("exComServiceImpl")
-	private ExComService exComService;
-	@Autowired
-	@Qualifier("dietComServiceImpl")
-	private DietComService dietComService;
-	@Autowired
-	@Qualifier("meetingServiceImpl")
-	private MeetingService meetingService;
-	
-	
-	public MainController() {
-		System.out.println(this.getClass());
-	}
-	
-	@Value("#{commonProperties['pageUnit']}")
-		int pageUnit;	
+   //Field
+   @Autowired
+   @Qualifier("mainServiceImpl")
+   private MainService mainService;
+   @Autowired
+   @Qualifier("exInfoServiceImpl")
+   private ExInfoService exInfoService;
+   @Autowired
+   @Qualifier("exComServiceImpl")
+   private ExComService exComService;
+   @Autowired
+   @Qualifier("dietComServiceImpl")
+   private DietComService dietComService;
+   @Autowired
+   @Qualifier("meetingServiceImpl")
+   private MeetingService meetingService;
+   
+   
+   public MainController() {
+      System.out.println(this.getClass());
+   }
+   
+   @Value("#{commonProperties['pageUnit']}")
+      int pageUnit;   
 
-	@Value("#{commonProperties['pageSize']}")
-		int pageSize;
-	
-	@RequestMapping(value="/", method=RequestMethod.GET)
-	public String getMainPage(@ModelAttribute("search") Search search, Model model) throws Exception{
-		
-		System.out.println("MainController Come in");
-		//?��?���? 추천?��?��, ?��?��/?��?�� 추천, ?��박한 ?��모임 Business Logic
-/*		Map<String, Object> ex= exComService.listExComRecom(search);	
-		Map<String, Object> diet = dietComService.listDietComRecom(search);
-		Map<String, Object> meeting = meetingService.listMeeting(search);
-		List<Post> exInfo = exInfoService.listExInfo(Weather);*/
-		
-		
-		
-/*		if(search.getCurrentPage()==0) {
-			search.setCurrentPage(1);
-		}
-		
-		search.setPageSize(pageSize);*/
-		
-		
-		
-		return "main.jsp" ;
-	}
-	
-	@RequestMapping(value="listSearchMain", method=RequestMethod.GET)
-	public String listSearchMain(@ModelAttribute Search search, Model model) throws Exception{
-		
-		if(search.getCurrentPage()==0) {
-			search.setCurrentPage(1);
-		}
-		search.setPageSize(1);
-		
-		//Business Logic
-		Map<String, Object> map = mainService.listSearchMain(search);
-		
-		Page resultPage = new Page(search.getCurrentPage(), ((Integer)map.get("totalCount")).intValue(), pageUnit, pageSize);
-		
-		//MV
-		model.addAttribute("list", map.get("list"));
-		model.addAttribute("resultPage", resultPage);
-		model.addAttribute("search", search);
-		
-		return "forward:/main/listSearchMain.jsp";
-			
-	}
-
-	@RequestMapping(value="profileUpload")
-    public void profileUpload(MultipartFile file, HttpServletRequest request, HttpServletResponse response) throws Exception{
-		System.out.println("111111");
+   @Value("#{commonProperties['pageSize']}")
+      int pageSize;
+   
+   @RequestMapping(value="/", method=RequestMethod.GET)
+   public String getMainPage(@ModelAttribute("search") Search search, Model model) throws Exception{
+      
+      System.out.println("MainController Come in");
     
-    	mainService.profileUpload(file, request, response);
-    } 	
-	
+/*      Map<String, Object> ex= exComService.listExComRecom(search);   
+      Map<String, Object> diet = dietComService.listDietComRecom(search);
+      Map<String, Object> meeting = meetingService.listMeeting(search);
+      List<Post> exInfo = exInfoService.listExInfo(Weather);*/
+      
+      
+      
+/*      if(search.getCurrentPage()==0) {
+         search.setCurrentPage(1);
+      }
+      
+      search.setPageSize(pageSize);*/
+      
+      
+      
+      return "main.jsp" ;
+   }
+   
+   @RequestMapping(value="listSearchMain", method=RequestMethod.GET)
+   public String listSearchMain(@ModelAttribute Search search, Model model) throws Exception{
+      
+      if(search.getCurrentPage()==0) {
+         search.setCurrentPage(1);
+      }
+      search.setPageSize(1);
+      
+      //Business Logic
+      Map<String, Object> map = mainService.listSearchMain(search);
+      
+      Page resultPage = new Page(search.getCurrentPage(), ((Integer)map.get("totalCount")).intValue(), pageUnit, pageSize);
+      
+      //MV
+      model.addAttribute("list", map.get("list"));
+      model.addAttribute("resultPage", resultPage);
+      model.addAttribute("search", search);
+      
+      return "forward:/main/listSearchMain.jsp";
+         
+   }
 
-	
+   @RequestMapping(value="profileUpload")
+    public void profileUpload(MultipartFile file, HttpServletRequest request, HttpServletResponse response) throws Exception{
+      System.out.println("111111");
+    
+       mainService.profileUpload(file, request, response);
+    }    
+   
+
+   
 }
